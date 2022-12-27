3450c93d71d3d934bdb4a56e58108281869158ca
muhammad nadzman nasri <2190768@alfateh.upnm.edu.my>
2022-12-27 15:58:33 +0800

    refactoring arabic into class

--- UnifiedNumber/UnifiedNumber/UnifiedNumber/Form1.cs
+++ UnifiedNumber/UnifiedNumber/UnifiedNumber/Form1.cs
@@ -41,55 +41,7 @@ namespace unifiednumber
             if (arabiccharbutton.Checked == true)
             {
                 MsgLabel.Text = "Message: ";
-                string DecStr = valuelabel.Text;
-                string ArbStr = "";
-
-                for (int c = 0; c < DecStr.Length; c++)
-                {
-                    if (DecStr[c] == '0')
-                    {
-                        ArbStr = ArbStr + "۰";
-                    }
-                    else if (DecStr[c] == '1')
-                    {
-                        ArbStr = ArbStr + "۱";
-                    }
-                    else if (DecStr[c] == '2')
-                    {
-                        ArbStr = ArbStr + "۲";
-                    }
-                    else if (DecStr[c] == '3')
-                    {
-                        ArbStr = ArbStr + "۳";
-                    }
-                    else if (DecStr[c] == '4')
-                    {
-                        ArbStr = ArbStr + "٤";
-                    }
-                    else if (DecStr[c] == '5')
-                    {
-                        ArbStr = ArbStr + "۵";
-                    }
-                    else if (DecStr[c] == '6')
-                    {
-                        ArbStr = ArbStr + "٦";
-                    }
-                    else if (DecStr[c] == '7')
-                    {
-                        ArbStr = ArbStr + "٧";
-                    }
-                    else if (DecStr[c] == '8')
-                    {
-                        ArbStr = ArbStr + "٨";
-                    }
-                    else if (DecStr[c] == '9')
-                    {
-                        ArbStr = ArbStr + "٩";
-                    }
-
-                }
-
-                answerlabel.Text = ArbStr;
+                answerlabel.Text = myunifiednumber.ToArabic();
             }
 
 
